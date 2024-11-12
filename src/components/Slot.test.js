425c1c90cf4618d63e9882eda199178ea9b24449
Mattia Richetto <mattiarichetto@Mattias-MacBook-Air.local>
2024-11-11 22:26:24 -0500

    fix snapshot

--- src/components/Slot.test.js
+++ src/components/Slot.test.js
@@ -1,9 +1,14 @@
+import { DOWN } from './constants';
 import Slot from './Slot';
 import { render } from '@testing-library/react';
 
 it('renders w/ knife', () => {
   const { asFragment } = render(
-    <Slot id="s1" knife={{ color: 'blue', id: 'k1' }} onClick={() => {}} />
+    <Slot
+      id="s1"
+      knife={{ color: 'blue', id: 'k1', state: DOWN }}
+      onClick={() => {}}
+    />
   );
   expect(asFragment()).toMatchSnapshot();
 });
